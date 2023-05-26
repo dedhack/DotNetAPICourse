c3ce5a778f9ba397d58f5752e11ccb897188caac
Izhar Bin Jumadi <eezhar@gmail.com>
2023-05-27 00:41:23 +0800

    types

---
+++ dotnet-course-code/HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, Yoooo!");
+
+
+using System;
+
+namespace HelloWorld // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello boiiis!");
+
+            // Variables
+            // Byte is for 8 bit integer
+            sbyte mySbyte = 127;
+            sbyte mySecondSbyte = -128;
+            byte myByte = 255;
+            byte mySecondByte = 0;
+
+            // 16 Bit integer
+            short myShort = 32767;
+            short mySecondShort = -32768;
+            ushort myUshort = 65535; // Unsigned short
+
+        }
+    }
+}
